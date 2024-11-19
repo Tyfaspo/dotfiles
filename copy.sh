b0edf7458a40042d367111ac4086d9ca54e0caff
Tyfaspo <svsh991@gmail.com>
2024-11-19 13:59:41 +0500

    script to sync with local files

---
+++ copy.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+USER=$(whoami)
+
+if [ "$USER" = "root" ]; then
+  echo "do not run as root"
+  exit 0
+fi
+
+CONFIG="/home/$USER/.config"
+
+FOLDERS=("kitty" "dunst" "hypr" "fastfetch" "nvim" "rofi" "waybar" "thefuck" "wlogout")
+
+# copy from .config
+for FOLDER in "${FOLDERS[@]}"; do
+  if [ -d "$CONFIG/$FOLDER" ]; then
+    echo "$FOLDER exists in .config"
+    cp -r "$CONFIG/$FOLDER" "./"
+  else
+    echo "$FOLDER does not exist in .config"
+  fi
+done
+
+# since i use .vimrc
+if [ -f "/home/$USER/.vimrc" ]; then 
+  cp "/home/$USER/.vimrc" "./"
+fi
+
+echo "DONE"
